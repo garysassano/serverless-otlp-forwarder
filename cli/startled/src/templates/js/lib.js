66a5f9bce0f06bcd11483e456693ad5960ee322a
Alessandro Bologna <alessandro.bologna@gmail.com>
2025-05-08 23:16:45 -0400

    feat: expand default color palette for charts in lib.js

--- cli/startled/src/templates/js/lib.js
+++ cli/startled/src/templates/js/lib.js
@@ -13,10 +13,27 @@
 
 // Default color palette for charts (can be customized)
 window.DEFAULT_COLOR_PALETTE = [
-    "#3fb1e3", "#6be6c1", "#626c91",
-    "#a0a7e6", "#c4ebad", "#96dee8"
+    "#2ec7c9",
+    "#b6a2de",
+    "#5ab1ef",
+    "#ffb980",
+    "#d87a80",
+    "#8d98b3",
+    "#e5cf0d",
+    "#97b552",
+    "#95706d",
+    "#dc69aa",
+    "#07a2a4",
+    "#9a7fd1",
+    "#588dd5",
+    "#f5994e",
+    "#c05050",
+    "#59678c",
+    "#c9ab00",
+    "#7eb00a",
+    "#6f5553",
+    "#c14089"
 ];
-
 // ===============================
 // Core UI and Setup Functionality
 // ===============================
@@ -44,7 +61,7 @@ function setTheme(theme) {
             lightIcon.style.display = 'block';
         }
     }
-    // Reinitialize chart with new theme
+    // // Reinitialize chart with new theme
     if (chart) {
         chart.dispose();
     }
